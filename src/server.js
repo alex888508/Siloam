4d46bbd013174c6793e845bc3e4e537e5ff1249a
alex888508 <45968999+alex888508@users.noreply.github.com>
2023-11-20 02:13:01 +0300

    Update server.js

--- src/server.js
+++ src/server.js
@@ -5,7 +5,7 @@ const mongoose = require('mongoose');
 const { MongoClient } = require('mongodb');
 
 const app = express();
-const port = 3000;
+const port = 3001;
 
 // Middleware to parse JSON requests
 app.use(bodyParser.json());
