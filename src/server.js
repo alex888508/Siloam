cc49e2ecb48b1065f65ddd8687600dd0a9784fee
alex888508 <45968999+alex888508@users.noreply.github.com>
2023-11-20 02:21:26 +0300

    Update server.js

--- src/server.js
+++ src/server.js
@@ -3,10 +3,23 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 const mongoose = require('mongoose');
 const { MongoClient } = require('mongodb');
-
+const path = require('path');
 const app = express();
 const port = 3001;
 
+
+
+
+app.use(express.static(path.join(__dirname, 'build')));
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'build', 'index.js'));
+});
+
+const PORT = process.env.PORT || 3001;
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
 // Middleware to parse JSON requests
 app.use(bodyParser.json());
 app.use(cors());
